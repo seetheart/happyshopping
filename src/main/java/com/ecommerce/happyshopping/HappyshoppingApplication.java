977ed7793f6b58003a9e5df8460d1841a3263633
siddharthsingh <siddharthsiddhu899@gmail.com>
2025-07-22 17:55:33 +0530

    first commit

---
+++ src/main/java/com/ecommerce/happyshopping/HappyshoppingApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.happyshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyshoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappyshoppingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/happyshopping/HappyshoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.happyshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyshoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
