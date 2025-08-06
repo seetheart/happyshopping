311a7af84b2cd1de8d59fd19bbb4fc91eaee68b3
siddharthsingh <siddharthsiddhu899@gmail.com>
2025-08-06 22:46:05 +0530

    Creating custom authentication

---
+++ src/main/java/com/ecommerce/happyshopping/security/services/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.ecommerce.happyshopping.security.services;
+
+import com.ecommerce.happyshopping.model.User;
+import com.ecommerce.happyshopping.repositories.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not found with username"));
+
+        return UserDetailsImpl.build(user);
+    }
+}
